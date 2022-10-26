281f445afe138272e398b3e978e2777840738422
kapil234 <kapil.kumar@samparksolutions.com>
2022-10-26 17:23:49 +0530

    shopping cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
+import {useContext} from 'react'
+import {Store} from '../utils/Store'
 
 const Layout = ({children, title}) => {
+  const {state, dispatch} = useContext(Store)
+  const {cart} = state
   return (
     <>
       <Head>
         <title>{title ? title + ' -Amazon' : 'Amazon'}</title>
-        <meta name='description' content='Ecommerce Website' />
+        <meta name='description' content='E-commerce Website' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <div className='flex min-h-screen flex-col justify-between '>
@@ -17,8 +21,15 @@ const Layout = ({children, title}) => {
               <a className='text-lg font-bold'>amazon</a>
             </Link>
             <div>
-              <Link href='/cart'>
-                <a className='p-2'>Cart</a>
+              <Link href='/Cart'>
+                <a className='p-2'>
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <span className='text-xs ml-1 rounded-full bg-red-600 px-2 py-1 font-bold text-white '>
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </span>
+                  )}
+                </a>
               </Link>
               <Link href='/login'>
                 <a className='p-2'>Login</a>
@@ -27,7 +38,9 @@ const Layout = ({children, title}) => {
           </nav>
         </header>
         <main className='container m-auto mt-4 px-4 '>{children}</main>
-        <footer className='flex justify-center items-center h-10 shadow-inner'>Copyright @ 2022 Amazon</footer>
+        <footer className='flex justify-center items-center h-10 shadow-inner'>
+          Copyright @ 2022 Amazon
+        </footer>
       </div>
     </>
   )
